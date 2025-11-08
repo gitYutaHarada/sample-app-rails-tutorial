fea8cec6d22177ffb329ee1d8ee68d69aaf0de88
haradayuuta <mayutokuyuta@gmail.com>
2025-11-08 19:09:34 +0900

    Add build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
-set -o nounset
-set -o pipefail
-
-# binstub整備（Windows由来のCRLF/ ruby.exe対策）
-chmod a+x bin/* || true
-sed -i "s/\r$//g" bin/* || true
-sed -i '1s|^#!/usr/bin/env ruby\.exe$|#!/usr/bin/env ruby|' bin/* || true
-
 bundle install
-
-# 本番鍵なしでアセットだけ先にプリコンパイル
-SECRET_KEY_BASE_DUMMY=1 bin/rails assets:precompile
-bin/rails assets:clean
-
-# ここではDBに触らない（RenderのPost-deployで実行）
-# bin/rails db:migrate
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
