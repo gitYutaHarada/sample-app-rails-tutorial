dc3a0760cccfc379c5c4ba86d4d8b388a8affd10
haradayuuta <mayutokuyuta@gmail.com>
2025-11-09 19:45:59 +0900

    Remove cached node_modules     before npm install on Render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,8 @@
 set -o errexit
 
 bundle install
+# Remove cached node_modules to ensure clean install for Linux platform
+rm -rf node_modules
 npm install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
