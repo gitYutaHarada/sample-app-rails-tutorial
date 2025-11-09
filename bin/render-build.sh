4e2766193547925c6016a035c70b146d6b532fb5
haradayuuta <mayutokuyuta@gmail.com>
2025-11-09 19:40:59 +0900

    Add npm install to render    build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+
 bundle install
+npm install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
